e00580ee9a4e8f13800f89785194132576d4a2a8
Ariel Galeppi <arielgaleppi@gmail.com>
2024-05-15 08:34:19 +0200

    sin errores

--- Drinkypedia/src/App.jsx
+++ Drinkypedia/src/App.jsx
@@ -1,16 +1,15 @@
 import {BrowserRouter,Routes,Route,Link} from "react-router-dom"
-<<<<<<< HEAD
-import { Home } from "./pages/home"
-import { Cocktails } from "./pages/cocktails"
-import { CocktailDetail } from "./pages/cocktailDetail" 
+
+
+import { CocktailDetail } from "./pages/CocktailDetail" 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-=======
+
 import { Home } from "./pages/Home"
 import { Cocktails } from "./pages/Cocktails"
-import { CocktailDetail} from "./pages/CocktailDetail" 
 
->>>>>>> 3a8b2ff761aff6b9c8d376415afea1a4f69821c1
+
+
 
 export const App = ()  =>{
   return (
